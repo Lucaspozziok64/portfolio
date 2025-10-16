aa2d70d21ca6614be21e2e6e57f23221cc2ff809
Lucasfi <lukafigueroa64@gmail.com>
2025-10-16 15:32:39 -0300

    maquete modal de poryectos y cree array de tecnologias en Inicio

--- src/components/ComponentsInicio/CardProyectos.jsx
+++ src/components/ComponentsInicio/CardProyectos.jsx
@@ -1,24 +1,75 @@
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+
 const CardProyectos = ({ proyecto }) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div className="col">
-      <a href={proyecto.proyectoUrl} target="_blank">
-        <div className="contenedorImagen">
-          <div>
-            <h3 className="text-center">{proyecto.title}</h3>
+    <>
+      <div className="col">
+        <Button onClick={handleShow} variant="white" className="bg-gradient">
+          <div className="contenedorImagen">
+            <div>
+              <h3 className="text-center">{proyecto.title}</h3>
+            </div>
+            <div className="container my-3">
+              <img
+                src={proyecto.pic}
+                alt={proyecto.title}
+                className="card-image img-fluid"
+              />
+            </div>
           </div>
-          <div className="container my-3">
+        </Button>
+      </div>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header className="text-end" closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            {proyecto.title}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{proyecto.descripcion}</p>
+          <div className="contenedorImagen border border-black">
             <img
               src={proyecto.pic}
               alt={proyecto.title}
               className="card-image img-fluid"
             />
           </div>
-          <p className="container-fluid text-decoration-none">
-            {proyecto.descripcion}
-          </p>
-        </div>
-      </a>
-    </div>
+          <hr></hr>
+          <div>
+            <h5 className="text-center my-2">Tecnologias empleadas:</h5>
+            <ul className="list-unstyled d-flex justify-content-evenly">
+              {proyecto.tecnologias.map((tecnologia, index) => (
+                <li key={index} className="my-1">
+                  <img
+                    src={tecnologia}
+                    alt={`Tecnología ${index + 1}`}
+                    className="imagenTecnologia"
+                  />
+                </li>
+              ))}
+            </ul>
+            <a
+              href={proyecto.proyectoUrl}
+              target="_blank"
+              className="d-block text-end text-dark"
+            >
+              Ver proyecto
+            </a>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
 
--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -16,24 +16,50 @@ const Inicio = () => {
       proyectoUrl: "https://recetasespeciales.netlify.app/",
       descripcion:
         "Helps users view or copy forgotten or unknown recipes, just by creating an account you have full access to view, read, create, delete",
+      tecnologias: [
+        "https://img.shields.io/badge/-React-61DAFB?logo=react&logoColor=white&style=flat",
+        "https://img.shields.io/badge/-Node.js-339933?logo=node.js&logoColor=white&style=flat",
+        "https://img.shields.io/badge/-MongoDB-47A248?logo=mongodb&logoColor=white&style=flat",
+        "https://img.shields.io/badge/-Express-000000?logo=express&logoColor=white&style=flat",
+        "https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascript&logoColor=black&style=flat",
+      ],
     },
     {
       id: 2,
       title: "Web Movies",
       pic: xtream,
       proyectoUrl: "https://moviesxtream.netlify.app/",
+      tecnologias: [
+        "https://img.shields.io/badge/-React-61DAFB?logo=react&logoColor=white&style=flat",
+        "https://img.shields.io/badge/-CSS3-1572B6?logo=css3&logoColor=white&style=flat",
+        "https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascript&logoColor=black&style=flat",
+        "https://img.shields.io/badge/-API-FF6C37?logo=api&logoColor=white&style=flat",
+      ],
     },
     {
       id: 3,
       title: "leaked news",
       pic: noticiasApp,
       proyectoUrl: "https://noticiasfiltradas.netlify.app/",
+      tecnologias: [
+        "https://img.shields.io/badge/-React-61DAFB?logo=react&logoColor=white&style=flat",
+        "https://img.shields.io/badge/-Bootstrap-7952B3?logo=bootstrap&logoColor=white&style=flat",
+        "https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascript&logoColor=black&style=flat",
+        "https://img.shields.io/badge/-CSS3-1572B6?logo=css3&logoColor=white&style=flat",
+      ],
     },
     {
       id: 4,
       title: "Mates Ecommerce Website",
       pic: mates,
       proyectoUrl: "https://mateandoshop.netlify.app/",
+      tecnologias: [
+        "https://img.shields.io/badge/-React-61DAFB?logo=react&logoColor=white&style=flat",
+        "https://img.shields.io/badge/-HTML5-E34F26?logo=html5&logoColor=white&style=flat",
+        "https://img.shields.io/badge/-CSS3-1572B6?logo=css3&logoColor=white&style=flat",
+        "https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascript&logoColor=black&style=flat",
+        "https://img.shields.io/badge/-Bootstrap-7952B3?logo=bootstrap&logoColor=white&style=flat",
+      ],
     },
   ];
   return (
