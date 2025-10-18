179abca927c08a11f79af45cc56b5ecdae8c2823
Lucasfi <lukafigueroa64@gmail.com>
2025-10-18 13:20:39 -0300

    Arrgle conflictos de carpeta

--- src/components/ComponentsInicio/ListaProyectos.jsx
+++ src/components/ComponentsInicio/ListaProyectos.jsx
@@ -4,7 +4,9 @@ const ListaProyectos = ({ proyectos }) => {
   return (
     <>
       <div className="container row row-cols-1 row-cols-md-2 g-4 my-2">
-        {proyectos.map((proyecto)=> (<CardProyectos key={proyecto.id} proyecto={proyecto} />))}
+        {proyectos.map((proyecto) => (
+          <CardProyectos key={proyecto.id} proyecto={proyecto} />
+        ))}
       </div>
     </>
   );
